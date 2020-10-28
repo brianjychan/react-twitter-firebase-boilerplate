a300671c3bd43be1e880d4eae9705fd767452595
Brian Chan <brianchan2489@gmail.com>
2020-10-28 01:08:13 -0700

    feat: add basic homepage layout with email/pw auth

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from 'react'
 import { useFirebase } from '../Firebase'
-import Button from 'react-bootstrap/Button'
 import { useSession } from '../Session'
 import { Redirect } from 'react-router-dom'
 import { ROUTES } from '../../constants'
 
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+
 import styles from './Home.module.css'
+import { joinStyles } from '../Utilities/joinStyles';
+
 
 const HomePage: React.FC = () => {
     const firebase = useFirebase()
     const session = useSession()
     const [twitterSignIn, setTwitterSignIn] = useState(false)
+
+    const openSessionUserPage = () => { }
 
     useEffect(() => {
         // Sample write to Firestore
@@ -34,24 +41,78 @@ const HomePage: React.FC = () => {
             <Redirect to={ROUTES.TWITTER} />
         )
     }
-    if (session.auth) {
-        return (
-            <div className={styles.window}>
-                <p>Home</p>
-                <p>Logged in!</p>
-                <p>Hi, {session.prof?.username}</p>
-                <Button variant="outline-primary" onClick={() => { firebase.doSignOut() }}>Sign Out</Button>
 
-            </div>
-        )
-    } else {
-        return (
-            <div className={styles.window}>
-                <p>Home</p>
-                <Button variant="primary" onClick={() => { setTwitterSignIn(true) }}>Sign In to Twitter</Button>
-            </div>
-        )
+    const sessionName = session.prof?.name ? session.prof.name : ''
+
+    return (
+        <Row className={styles.window}>
+            <Col>
+                {/* Navbar */}
+                <Row>
+                    <Col xs="auto">
+                        <Row className={"align-items-end"}>
+                            {/* Title */}
+                            <Col md={"auto"} className={styles.noPaddingRight}>
+                                <h1 className={joinStyles("colorGreen", styles.header)}>{"Cool Website™"}</h1>
+                            </Col>
+                            {session.auth?.uid && <Col xs="auto">
+                                <span className={joinStyles(styles.feat, "colorGray")}>
+                                    (with
+                                    <span className={joinStyles("colorGreen", styles.userName)} onClick={openSessionUserPage}>
+                                        {" " + sessionName}
+                                    </span>
+                                    )
+                                </span>
+                            </Col>}
+                        </Row>
+                    </Col>
+                </Row >
+                {/* Main Content */}
+                <Row className={styles.marginTop}>
+                    <Col>
+                        {session.auth ?
+                            <>
+                                <p>Logged in!</p>
+                                <Button variant="outline-primary" onClick={() => { firebase.doSignOut() }}>Sign Out</Button>
+                            </>
+                            :
+                            <>
+                                <Button variant="primary" onClick={() => { setTwitterSignIn(true) }}>Sign In with Twitter</Button>
+                                <p>or...</p>
+                                <EmailPwAuth />
+                            </>
+                        }
+                    </Col>
+                </Row>
+            </Col>
+        </Row>
+    )
+}
+
+const EmailPwAuth: React.FC = () => {
+    const firebase = useFirebase()
+
+    const [inputs, setInputs] = useState({ email: '', pw: '' })
+    const { email, pw } = inputs
+    // Handle text box change
+    const handleInputs = (event: React.ChangeEvent<HTMLInputElement>) => {
+        event.persist()
+        setInputs(prev => ({ ...prev, [event.target.name]: event.target.value }))
     }
+    // Listen for enter key
+    const listenEnterKey = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            firebase.doSignInWithEmailAndPassword(email, pw)
+        }
+    }
+
+    return (
+        <div>
+            <input name='email' onChange={handleInputs} className={styles.marginTopWidth100PaddingSmall} value={inputs.email} placeholder="email" onKeyDown={listenEnterKey} />
+            <input name='pw' onChange={handleInputs} className={styles.marginTopWidth100PaddingSmall} value={inputs.pw} placeholder="password" onKeyDown={listenEnterKey} />
+        </div>
+
+    )
 }
 
 export { HomePage }
